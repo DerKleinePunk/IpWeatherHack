cbcafc417f7edc2a3252b40de39498891b1879a2
Hydroo ! <nivek2006@web.de>
2024-07-14 19:07:32 +0200

    fix(map): added weatherData_loaded event

--- visu/map.js
+++ visu/map.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const map = L.map("map").setView([52.52, 13.41], 13);
+  document.addEventListener("weatherData_loaded", () => {
+    const map = L.map("map").setView([52.52, 13.41], 13);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-  weatherData.forEach((data) => {
-    L.circle([data.latitude, data.longitude], {
-      color: "red",
-      fillColor: "#f03",
-      fillOpacity: 0.5,
-      radius: 500,
-    })
-      .addTo(map)
-      .bindPopup(`IP: ${data.ip}<br>Timestamp: ${data.timestamp}`);
+    weatherData.forEach((data) => {
+      L.circle([data.latitude, data.longitude], {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 500,
+      })
+        .addTo(map)
+        .bindPopup(`IP: ${data.ip}<br>Timestamp: ${data.timestamp}`);
+    });
   });
 });
